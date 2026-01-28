a38160ba90c81c3f6dd46eafbb6436c3bca07c20
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-01-28 14:25:04 +0100

    Upload Esempio 1

---
+++ LaboratorioMAUI/AppEsempio/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AppEsempio
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else if (count % 100 == 0)
+                CounterBtn.Text = $"Congratulations  you got a multiple of 100!";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
