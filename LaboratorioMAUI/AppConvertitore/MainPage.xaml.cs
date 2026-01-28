007f8f23bf1c4c4c62dbe715eec287f93d634445
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-01-28 15:42:07 +0100

    Upload

---
+++ LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Java.Util.Concurrent;
+
+namespace AppConvertitore
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {   
+            // Inizializza i componenti grafici
+            InitializeComponent();
+        }
+
+        private void btnConverti_Clicked(object sender, EventArgs e)
+        {
+            string valorImporto = entConversione.Text;
+            double franchi = Convert.ToDouble(valorImporto);
+
+            double euro = franchi * 1.07;
+
+            lblRisultato =
+        }
+    }
+
+}
