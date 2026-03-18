cd2c3ea604e2fbc930f578fa38a242766191b733
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-03-18 15:43:28 +0100

    upload (as always)

---
+++ LaboratorioMAUI/AppDiario/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace AppDiario
+{
+    public partial class MainPage : ContentPage
+    {
+        string percorsoFile = Path.Combine(FileSystem.AppDataDirectory, "note.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSalvaClicked(object sender, EventArgs e)
+        {
+        }
+        private void OnLeggiClicked(object sender, EventArgs e)
+        {
+        }
+    }
+
+}
