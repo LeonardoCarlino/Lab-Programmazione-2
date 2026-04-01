2c680c70f7cf4237c7f002e201dee912656cc5cb
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-04-01 15:33:19 +0200

    Upload

---
+++ AppSpeseCorrezione/AppSpeseCorrezione/Models/VoceBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSpeseCorrezione.Models
+{
+    // Super Classe (base) astratta
+    public abstract class VoceBase
+    {   
+        // variabile --> campo
+        private string _descrizione;
+
+        // Propfull (in java getter e setter)
+        public string Descrizione
+        {
+            get { return _descrizione; }
+            set { _descrizione = value; }
+        }
+
+        public abstract string ToRiga();
+
+    }
+}
