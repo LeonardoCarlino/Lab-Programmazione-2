c77e1f3469417ccc91680e37fb8eb3f9791cce66
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-05-13 15:41:57 +0200

    Upload esercizi

--- EsempiTeoria/HelloLinq/HelloLinq/Program.cs
+++ EsempiTeoria/HelloLinq/HelloLinq/Program.cs
@@ -10,21 +10,57 @@ namespace HelloLinq
     {
         static void Main(string[] args)
         {
-            List<int> numeri = new List<int> {1 ,2, 3, 4, 5, 6 };
-            List<Studente> studenti = new List<Studente>();
-            studenti.Add(new Studente("Giuseppe", 134));
-            studenti.Add(new Studente("Simona", 105));
+            List<Student> students = Student.GetStudents();
 
-            // vogliamo estrapolare i numeri pari della lista numeri
-            // ToList() trasforma l'oggetto Ienumerable in lista
-            List<int> pari = numeri.Where(x => x % 2 == 0).ToList();
+            // Es1
 
-            var nomi = studenti.Select(x => x.Nome).ToList();
+            List<Student> es1 = students.Where(s => s.Grade >= 70).ToList();
+            foreach(Student s in es1)
+            {
+                Console.WriteLine(s.Grade);
+            }
 
-            foreach(string s in nomi)
+            // Es2
+            List<Student> es2 = students.Where(s => s.Branch == "Informatica").ToList();
+            foreach (Student s in es2)
             {
                 Console.WriteLine(s);
             }
+
+            // Es3
+            bool es3 = students.Any(s => s.Grade == 100);
+            Console.WriteLine(es3);
+
+            // Es4
+            bool es4 = students.All(s => s.Grade > 40);
+            Console.WriteLine(es4);
+
+            // Es5
+            Student es5 = students.FirstOrDefault(s => s.Branch == "Telecomunicazioni");
+            Console.WriteLine(es5);
+
+            // Es6
+            Student es6 = students.SingleOrDefault(s => s.ID == 1001);
+            Console.WriteLine(es6);
+
+
+            // Es7
+            List<string> es7 = students.Select(s => s.Name).ToList();
+            foreach(string student in es7)
+            {
+                Console.WriteLine(student);
+            }
+
+            // Es8
+            List<string> es8 = students.Select(s => s.Name.ToUpper()).ToList();
+
+            // Es9
+            List<string> es9 = students.Select(s => s.Name).ToList();
+
+
+
+
+
         }
     }
 }
