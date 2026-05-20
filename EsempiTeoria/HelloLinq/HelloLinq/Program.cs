099b594a13386a971865181c8ec016914fd9c571
LeonardoCarlino <jjo021@student.edu.ti.ch>
2026-05-20 15:19:40 +0200

    Upload esercizi Linq

--- EsempiTeoria/HelloLinq/HelloLinq/Program.cs
+++ EsempiTeoria/HelloLinq/HelloLinq/Program.cs
@@ -62,9 +62,109 @@ namespace HelloLinq
                     s.Grade,
                 });
 
+            // Es 10
+            int es10 = students.Count(s => s.Branch == "Informatica");
 
+            // Es11
+            double es11 = students .Average(s => s.Grade);
 
+            // Es12
+            int es12 = students.Max(s => s.Grade);
 
+            // Es13
+            int es13 = students.Min(s => s.Grade);
+
+            // Es14
+            int es14 = students .Sum(s => s.Grade);
+
+            Console.WriteLine("\nCAT 6 -------------------" + "\n" + es10 + "\n" + es11 + "\n" + es12 + "\n" + es13 + "\n" + es14 + "-----------------------");
+
+            // Es15
+            var es15 = students
+                .OrderByDescending(s => s.Grade);
+
+            // Es16
+            var es16 = students
+                .OrderBy(s => s.Branch)
+                .ThenBy(s => s.Name);
+
+            // Es17
+            var es17 = students
+                .OrderByDescending(s => s.Grade)
+                .Take(3);
+
+            // Es18
+            var es18 = students
+                .GroupBy(s => s.Subject);
+
+            // Es19
+            var es19 = students
+                .GroupBy(s => s.Subject)
+                .Select(g => new
+                {
+                    Subject = g.Key,
+                    StudentCount = g.Count(),
+                    AverageGrade = g.Average(s => s.Grade)
+                });
+
+            // Es20
+            var es20 = students
+                .SelectMany(s => s.Programming);
+
+            // Es21
+            var es21 = students 
+                .SelectMany (s => s.Programming)
+                .Distinct();
+
+            // Es22
+            var es22 = students
+                .SelectMany(s => s.Programming)
+                .Distinct()
+                .OrderBy(language => language);
+
+            // Es23
+            bool es23 = students
+                .SelectMany(s => s.Programming)
+                .Contains("C#");
+
+            // Es 24
+            List<Student> es24 = students
+                .Where(s => s.Grade > 80)
+                .ToList();
+
+            // Es25
+            string[] es25 = students
+                .Select(s => s.Name)
+                .ToArray();
+
+            // Es26
+            foreach(var name in students.Select(s => s.Name))
+            {
+                Console.WriteLine(name);
+            }
+
+            // Es27
+            var es27 = students
+                .FirstOrDefault(s => s.ID == 999);
+
+            // Es28
+            string es28 = es27?.Name;
+
+            // Es29
+            string es29 = es27?.Name ?? "Id non trovato...";
+
+            // Es30
+            var es30 = students
+                .Where(s => s.Grade > 70)
+                .OrderByDescending(s => s.Grade)
+                .Take(3)
+                .Select(s => s.Name);
+
+            // Es31
+            var es31 = students
+                .SelectMany(s => s.Programming)
+                .Distinct()
+                .OrderBy(language => language);
 
         }
     }
